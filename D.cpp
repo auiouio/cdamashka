555c1bfd087ec1da709edc0cd003dcf196e7e9ae
auiouio <117065812+auiouio@users.noreply.github.com>
2023-02-04 23:58:39 +0300

    Update D.cpp

--- D.cpp
+++ D.cpp
@@ -7,12 +7,12 @@ int main() {
     int x = 0, y = 0, nod = 0;
     cin >> x >> y;
     while (x != y) {
-    if (x>y) {
-      x = x-y;
+        if (x>y) {
+          x = x-y;
+        }
+        else {
+          y = y-x;
+        }
     }
-    else {
-      y = y-x;
-    }
-  }
-  cout << x;
+    cout << x;
 }
