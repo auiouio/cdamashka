1fb5c73ba16c12713ada6dde85068f8341b28741
auiouio <117065812+auiouio@users.noreply.github.com>
2023-02-04 22:44:51 +0300

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a = 0, b = 0, c = 0, D = 0;
+    float x_1 = 0, x_2 = 0;
+    cin >> a >> b >> c;
+    D = b*b - 4*a*c;
+    x_1 = (-b + sqrt(D))/(2*a);
+    x_2 = (-b - sqrt(D))/(2*a);
+    if (D>0) {
+        if (x_1 > x_2) {
+        cout << x_2 << ' ' << x_1 << " D ne 0" << endl;
+    }
+    else if (x_1 < x_2) {
+        cout << x_1 << ' ' << x_2 << " D не 0" << endl;
+    }
+    }
+    else if (D == 0) {
+        cout << x_2 << endl;
+    }
+    else if (D < 0) {
+        cout << '\n' << endl;
+    }
+    return 0;
+}
