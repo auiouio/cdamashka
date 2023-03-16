4ebe3aa8a5ae6495079ee8ee81ff4cdb1d11ae04
auiod <katia.zavieiboroda@mail.ru>
2023-03-16 13:27:36 +0300

    работает но надо что то с указательной арифметикой сделать

---
+++ week05/B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short int* ptr = new short int[10];
+    for (int i = 0; i < 10; ++i) {
+        ptr[i] = i;
+    }
+    for (int i = 0; i < 10; ++i) {
+        cout << &ptr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << ptr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i+=2) {
+        ptr[i] = i*i;
+    }
+    for (int i = 0; i < 10; ++i) {
+        cout << ptr[i] << " ";
+    }
+    return 0;
+}
